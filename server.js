8fd307be826d5dcf8f26b1ade93788144c091c97
cseswapon <cseswaponsaha@gmail.com>
2025-02-03 21:28:13 +0600

    [MOD] - modify server listening code

--- server.js
+++ server.js
@@ -54,4 +54,4 @@ app.post("/chat", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT} `));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
